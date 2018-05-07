f29a4dd162007ee45b592b3a072b1e8bb18810d1
Forrest Parker <forrestwparker@gmail.com>
2018-05-07 15:46:00 -0700

    Completed Section 2: Preparing for action

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -7,9 +7,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tap: UIButton!
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
+
     }
 
     override func didReceiveMemoryWarning() {
